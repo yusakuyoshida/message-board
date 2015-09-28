2cac789ca8484f4eea380f140f27e4d42de820a6
yusakuyoshida <yusaku.ysd@gmail.com>
2015-09-28 15:08:31 +0000

    Imprementation of the validation

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,18 +1,24 @@
 class MessagesController < ApplicationController
-  def index
-    @messages=Message.all
-  end
-  
+ def index
+   @messages=Message.all
+   @message=Message.new
+ end
   
   def create
     @message=Message.new(message_params)
-    @message.save
+    if @message.save
     redirect_to root_path , notice: "メッセージを保存しました"
+  
+    else
+      @messages=Message.all
+      flash.now[:alert]="メッセージの保存に失敗しました。"
+      render "index"
+    end
   end
   
   private
   def message_params
     params.require(:message).permit(:name, :body)
   end
-  
+      
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,2 +1,4 @@
 class Message < ActiveRecord::Base
+  validates :name, length:{maximum:20}, presence:true
+  validates :body, length:{minimum:2, maximum:30}, presence:true
 end
