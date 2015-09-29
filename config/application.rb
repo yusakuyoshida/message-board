86a7849f4cf12399e843bf3af99161f09618fe86
yusakuyoshida <yusaku.ysd@gmail.com>
2015-09-29 08:19:49 +0000

    render form and revise of message-error

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,6 @@ module MessageBoard
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.action_view.field_error_proc = proc { |html_tag, instance| "<div class='has-error'>#{html_tag}</div>".html_safe }
   end
 end
