83e8b83ee190a3dce418e715794b815d0c39ac6d
yusakuyoshida <yusaku.ysd@gmail.com>
2015-10-01 11:29:23 +0000

    challenge2

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,5 @@
 class Message < ActiveRecord::Base
   validates :name, length:{maximum:20}, presence:true
   validates :body, length:{minimum:2, maximum:30}, presence:true
+  validates :age, numericality:{only_integer:true, greater_than_or_equal_to:0}, presence:true
 end
